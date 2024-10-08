b1cc3dadcb9fc302001808240d00a9a644b43f9a
Abduallah-Shaklaoon <abduallah.shaklaoon@edu.sait.ca>
2024-10-08 15:07:28 -0600

    Changed styling on the counter

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -14,10 +14,10 @@ export default function Counter() {
     };
     
     return (
-        <div className='bg-black border-2 border-gray-500 text-white w-24 shadow-2xl rounded-xl'>
-            <p className="mt-5 ml-3">Count {count}</p>
+        <div className='bg-black border-2 border-gray-500 text-white w-36 shadow-2xl rounded-xl flex-column align-middle m-auto'>
+            <p className="mt-5 ml-10">Count {count}</p>
             <div className='flex-row mt-5 mb-5'>
-                <button onClick={increment} disabled={count == 20} className='mr-2 ml-4 pr-2 pl-2 rounded border-solid border-2 disabled:bg-orange-300 disabled:border-none border-green-600 hover:bg-green-600 bg-slate-500'>+</button>
+                <button onClick={increment} disabled={count == 20} className='mr-2 ml-9 pr-2 pl-2 rounded border-solid border-2 disabled:bg-orange-300 disabled:border-none border-green-600 hover:bg-green-600 bg-slate-500'>+</button>
                 <button onClick={decrement} disabled={count == 1} className='ml-2 pr-2 pl-2 rounded border-solid border-2 bg-slate-500 border-red-600 hover:bg-red-600 disabled:bg-orange-300 disabled:border-none'>-</button>
             </div>
        </div>
