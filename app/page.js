7bd69450050512efce3e8119903dd02e1b4f34b5
Abduallah-Shaklaoon <abduallah.shaklaoon@edu.sait.ca>
2024-11-19 18:36:26 -0700

    Week 10

--- app/page.js
+++ app/page.js
@@ -1,31 +1,37 @@
 import Link from "next/link";
 
 export default function Page() {
-    return (
-        <main>
-            <ul>
-              <li>
-                <Link href="/week-2">Week 2 - Assignment</Link>
-              </li>
-              <li>
-                <Link href="/week-3">Week 3 - Shopping List</Link>
-              </li>
-              <li>
-                <Link href="/week-4">week-4</Link>
-              </li>
-              <li>
-                <Link href="/week-5">week-5</Link>
-              </li>
-              <li>
-                <Link href="/week-6">week-6</Link>
-              </li>
-              <li>
-                <Link href="/week-7">week-7</Link>
-              </li>
-              <li>
-                <Link href="/week-8/Class-Example">week-8</Link>
-              </li>
-            </ul>
-        </main>
-    );
+  return (
+    <main>
+      <ul>
+        <li>
+          <Link href="/week-2">Week 2 - Assignment</Link>
+        </li>
+        <li>
+          <Link href="/week-3">Week 3 - Shopping List</Link>
+        </li>
+        <li>
+          <Link href="/week-4">week-4</Link>
+        </li>
+        <li>
+          <Link href="/week-5">week-5</Link>
+        </li>
+        <li>
+          <Link href="/week-6">week-6</Link>
+        </li>
+        <li>
+          <Link href="/week-7">week-7</Link>
+        </li>
+        <li>
+          <Link href="/week-8/Class-Example">week-8</Link>
+        </li>
+        <li>
+          <Link href="/week-9-demo">week-9</Link>
+        </li>
+        <li>
+          <Link href="/week-10">week-10</Link>
+        </li>
+      </ul>
+    </main>
+  );
 }
